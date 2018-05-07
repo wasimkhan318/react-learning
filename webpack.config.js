694363c237dd02ada2d2a3fa3b3d99d21735f7aa
wasimkhan318 <wasim.khan318@gmail.com>
2018-05-07 17:30:53 +0530

    upgraded webpack to 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,15 @@
 const path = require('path');
 
 const config = {
-  resolve: {
-    modules: [
-      path.resolve('./lib'),
-      path.resolve('./node_modules')
-    ]
-  },
-  entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  
+  entry: './lib/component/index.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
+      { test: /\.js$/, use: 'babel-loader' }
     ]
   }
 };
