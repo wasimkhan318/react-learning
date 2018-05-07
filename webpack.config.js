6e40db548faecaca314ea5784acf77a5e6fa1496
wasimkhan318 <wasim.khan318@gmail.com>
2018-05-07 23:17:45 +0530

    Working sample app with webpack4

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require('path');
 
 const config = {
   
-  entry: './lib/component/index.js',
+  entry: './lib/components/index.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   module: {
     rules: [
-      { test: /\.js$/, use: 'babel-loader' }
+      { test: /\.js$/, exclude:'/node_modules', use: 'babel-loader' }
     ]
   }
 };
