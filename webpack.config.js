d9b2d478375505a5ea14254f5076e46595743ffa
wasimkhan318 <wasim.khan318@gmail.com>
2018-05-13 05:38:37 +0530

    update css for toggle

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,24 @@ const config = {
     filename: 'bundle.js'
   },
   module: {
-    rules: [
-      { test: /\.js$/, exclude:'/node_modules', use: 'babel-loader' }
+    rules:
+    [
+      { test: /\.js$/, exclude:'/node_modules', use: 'babel-loader' },
+      { test: /\.css$/, use: 'style-loader' },
+      { test: /\.css$/, use: 'css-loader' },
+      {
+            test: /\.(png|jp(e*)g|svg)$/,  
+            use: [{
+                loader: 'url-loader',
+                options: { 
+                    limit: 8000, // Convert images < 8kb to base64 strings
+                    name: 'images/[hash]-[name].[ext]'
+                } 
+            }]
+        }
+      
     ]
+    
   }
 };
 
